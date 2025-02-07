7527b8d9e4e6b32b489b35e14b6569df035b8a68
Sebastian Solano <sebaxsolanom@gmail.com>
2025-02-07 17:46:54 -0500

    logging functionality

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,35 @@
-console.log("Hello node!");
+import express from "express";
+import http from "http";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+import compression from "compression";
+import cors from "cors";
+import mongoose from "mongoose";
+import router from "./router";
+
+const app = express();
+
+app.use(
+  cors({
+    credentials: true,
+  })
+);
+
+app.use(compression());
+app.use(cookieParser());
+app.use(bodyParser.json());
+
+const server = http.createServer(app);
+
+server.listen(7070, () => {
+  console.log("Server running on http://localhost:7070");
+});
+
+const MONGO_URL =
+  "mongodb+srv://sebaxsolanom:accor0101@cluster0.vqoyq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+mongoose.Promise = Promise;
+mongoose.connect(MONGO_URL);
+mongoose.connection.on("error", (error: Error) => console.log(error));
+
+app.use("/", router());
